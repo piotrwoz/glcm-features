b18596a5f186bfb97e77d8cc423730900998f47e
piotrwoz <pio_wozniak@yahoo.com>
2024-01-12 13:37:05 +0100

    Init CMake project. Edit .gitignore

---
+++ glcm-features/glcm-features/glcm-features.cpp
@@ -0,0 +1,12 @@
+﻿// glcm-features.cpp: definiuje punkt wejścia dla aplikacji.
+//
+
+#include "glcm-features.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
